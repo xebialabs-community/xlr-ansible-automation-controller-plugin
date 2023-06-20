e35d4677030e4f1c3851ac62059e9cf6f0ea7489
droberts2013 <droberts@xebialabs.com>
2023-06-20 16:20:53 -0400

    Add extra_vars map, job tags, credentials

--- src/ansibletower/launchAndWait.py
+++ src/ansibletower/launchAndWait.py
@@ -26,6 +26,15 @@ from ansibletower import AAPServer
 import time
 import sys
 
+def create_payload(extra_vars, jobTags, credentials):
+    body = {}
+    body['extra_vars'] = extra_vars
+    body['job_tags'] = ",".join(jobTags)
+    body['credentials'] = credentials
+    body_as_string = json.dumps(body)
+    print "Body returned from create_payload() is %s" % body_as_string
+    return body_as_string
+
 if not ansibletower:
     print("Ansible Tower Server  must be provided")
     sys.exit(1)
@@ -40,26 +49,27 @@ headers = ansible_instance.create_header(request)
 
 if not isWorkflow:
   api_url = '/api/v2/job_templates/%s/launch/' % job_template_id
-  body = { "extra_vars": extra_vars}
-  response = request.post(api_url,body= json.dumps(body),contentType="application/json",headers=headers)
+  # body = { "extra_vars": extra_vars}
+  body_as_string = create_payload(extra_vars, jobTags, credentials)
+  response = request.post(api_url,body=body_as_string,contentType="application/json",headers=headers)
   result = json.loads(response.response)
   # Check the response status code to make sure the request was not successful.
-  
+
   if not response.isSuccessful():
       print("Failed to run the job template. Server return [%s], with content [%s]" % (response.status, response.response))
       sys.exit(1)
-  
+
   job_id= result["id"]
-  
+
   print("```")
   print(">>> Job template launched with job id  " + str(job_id))
   print("```")
   print("\n")
-  
+
   num_tries = 0
   api_url =  '/api/v2/jobs/%s/' % job_id
 
-  
+
   if max_retries:
       while num_tries < int(max_retries):
       # Define the URL for the Ansible Tower API endpoint to launch a job.
@@ -75,12 +85,12 @@ if not isWorkflow:
               time.sleep(float(wait_interval))
           else:
               raise Exception("Failed !. Server return [%s], with content [%s]" % (response.status, response.response))
-  
+
       if num_tries == int(max_retries):
           # maximum number of tries reached, handle the error
           raise Exception("Error: maximum number of tries reached")
       else:
-          # task completed 
+          # task completed
           print("```")
           print(">>> Task completed after " + str(num_tries) + " tries with status "+ status)
           print("```")
@@ -93,7 +103,7 @@ if not isWorkflow:
           print("* [>>> Job %s Link](%s/#/jobs/%s) <<<" % (str(job_id), ansibletower['url'], str(job_id)))
           print("\n")
           result= job_output
-  
+
   else:
       while True:
       # Define the URL for the Ansible Tower API endpoint to launch a job.
@@ -122,22 +132,23 @@ if not isWorkflow:
       result= job_output
 else:
   api_url = '/api/v2/workflow_job_templates/%s/launch/' % job_template_id
-  body = { "extra_vars": extra_vars}
-  response = request.post(api_url,body= json.dumps(body),contentType="application/json",headers=headers)
+#   body = { "extra_vars": extra_vars}
+  body_as_string = create_payload(extra_vars, jobTags, credentials)
+  response = request.post(api_url,body=body_as_string,contentType="application/json",headers=headers)
   result = json.loads(response.response)
   # Check the response status code to make sure the request was not successful.
-  
+
   if not response.isSuccessful():
       print("Failed to run the workflow job template. Server return [%s], with content [%s]" % (response.status, response.response))
       sys.exit(1)
-  
+
   job_id= result["id"]
-  
+
   print("```")
   print(">>> Workflow job template launched with job id  " + str(job_id))
   print("```")
   print("\n")
-  
+
   num_tries = 0
   api_url =  '/api/v2/workflow_jobs//%s/' % job_id
   if max_retries:
@@ -155,12 +166,12 @@ else:
               time.sleep(float(wait_interval))
           else:
               raise Exception("Failed !. Server return [%s], with content [%s]" % (response.status, response.response))
-  
+
       if num_tries == int(max_retries):
           # maximum number of tries reached, handle the error
           raise Exception("Error: maximum number of tries reached")
       else:
-          # task completed 
+          # task completed
           print("```")
           print(">>> Task completed after " + str(num_tries) + " tries with status "+ status)
           print("```")
@@ -198,9 +209,7 @@ else:
       print("\n")  # end markdown code block
       print("* [>>> Job %s Link](%s/#/jobs/workflow/%s/output) <<<" % (str(job_id), ansibletower['url'], str(job_id)))
       print("\n")
-      result= job_output      
+      result= job_output
 
 if stopOnFailure and not status == 'successful':
     raise Exception("Job status is "+ status)
-
-
